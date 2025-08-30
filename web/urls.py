cbe2dd89f69b2c99f336a75573b4a7648e78f0ce
Shmuel-Smadar <34glass43@gmail.com>
2025-08-30 15:47:28 +0300

    remove redundent function

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views_auth import index, login_start, oauth_callback, show_token
-from .views_files import download_by_project_name
 from .views_report import report_csv
 
 urlpatterns = [
@@ -8,6 +7,5 @@ urlpatterns = [
     path("auth/login/", login_start, name="login_start"),
     path("auth/callback/", oauth_callback, name="oauth_callback"),
     path("token/", show_token),
-    path("download", download_by_project_name, name="download_by_project_name"),
     path("report.csv", report_csv, name="report_csv"),
 ]
